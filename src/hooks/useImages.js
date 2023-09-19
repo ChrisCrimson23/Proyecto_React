90d4a9f7d8f7f74f001c034bb3d3d36b3c4a6467
ChrisCrimson23 <christrabajo98otmail.com>
2023-09-18 21:57:21 -0600

    Uso de Hooks

---
+++ src/hooks/useImages.js
@@ -0,0 +1,7 @@
+import Imagenes from '../lib/images'
+
+function useImages (tipos) {
+    return {data:Imagenes [tipos]} 
+}
+
+export default useImages
---
+++ src/lib/images.js
@@ -0,0 +1,11 @@
+const Images = {tipos:[{src: "https://img.automexico.com/2020/07/09/a87be74b-ef2f.jpg", alt: "Video_1", className: "Autos_1"},
+{src: "https://img.automexico.com/2020/05/12/i8InHrze/express-1-c2f3.jpg", alt: "Video_2", className: "Autos_1"},
+{src: "https://enterprise.mx/images/Van-Express-L.png", alt: "Video_3", className: "Autos_1"},
+{src: "https://images.latamautos.com/thumbs/w/450x265xC/ptx_migration_mexico/762023/4442005/o_o/pt_4442005_72850840.jpg?silly_me=1", alt: "Video_4", className: "Autos_2"},
+{src: "https://img.automexico.com/2020/05/12/i8InHrze/express-1-c2f3.jpg", alt: "Video_5", className: "Autos_2"},
+{src: "https://www.automotoradelsur.cl/wp-content/uploads/2023/04/auto-oferta-Kia-rio.jpg", alt: "Video_6", className: "Autos_2"},
+{src: "https://www.portalautomotriz.com/sites/portalautomotriz.com/files/media/photos/00006513-original.jpeg", alt: "Video_7", className: "Autos_3"},
+{src: "https://s3.eu-west-2.amazonaws.com/transeop/Blog/camion-rigido.jpeg", alt: "Video_8", className: "Autos_3"},
+{src: "https://www.casanovarentacar.mx/wp-content/uploads/2019/07/renta-de-camioneta-350.jpg", alt: "Video_9", className: "Autos_3"},]}
+
+export default Images
